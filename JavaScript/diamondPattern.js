e7b14a361788cf81f09aabc9da2e9cb4004ff99c
Ajid Abdulrahman <abdulrahmanajid@gmail.com>
2025-10-03 18:41:36 +0100

    diamondPattern.js

--- JavaScript/diamondPattern.js
+++ JavaScript/diamondPattern.js
@@ -1,51 +1,10 @@
-const readline = require("readline");
+// Quick usage
+DiamondPattern.generateQuickDiamond(5, "♦");
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+// Interactive mode
+const diamond = new DiamondPattern();
+diamond.start();
 
-rl.question("Enter width of diamond: ", function (input) {
-  const diamondWidth = parseInt(input);
-
-  // Check input constraints
-  if (diamondWidth < 1 || diamondWidth > 200) {
-    console.log("Invalid input. Please enter a value between 1 and 200.");
-    rl.close();
-    return;
-  }
-
-  // Function to generate spaces
-  const generateSpaces = (count) => " ".repeat(count);
-
-  // Function to generate hash symbols
-  const generateHashes = (count) => "# ".repeat(count);
-
-  // Print upper half of diamond
-  for (let i = 0; i < diamondWidth; i++) {
-    const spacesCount = diamondWidth - i - 1;
-    const hashesCount = i + 1;
-    console.log(generateSpaces(spacesCount) + generateHashes(hashesCount));
-  }
-
-  // Print lower half of diamond
-  for (let i = diamondWidth - 2; i >= 0; i--) {
-    const spacesCount = diamondWidth - i - 1;
-    const hashesCount = i + 1;
-    console.log(generateSpaces(spacesCount) + generateHashes(hashesCount));
-  }
-
-  rl.close();
-});
-
-
-/*
-Sample output for width 3:
-
-      # 
-     # # 
-    # # # 
-     # # 
-      # 
-      
-*/
+// Advanced patterns
+const advanced = new AdvancedDiamondPattern();
+advanced.generateFibonacciDiamond(6);
